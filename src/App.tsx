f6258cc9e42ce87bde597ae774206e59c34c3655
d4niee <daniellambrecht10@gmail.com>
2025-03-29 15:37:45 +0100

    initial project commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <a href='https://kube-ui.daniel-lambrecht-dev.de/'>Kubernetes Dashboard</a>
+        <br></br>
+        <a href='https://blog.daniel-lambrecht-dev.de/'>Blog</a>
+      </div>
+    </>
+  )
+}
+
+export default App
